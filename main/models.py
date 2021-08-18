65e11168768ec29bad3c58ca78e5b7e8bdaa8392
Ifihan <victoriaolusheye@gmail.com>
2021-08-18 10:12:57 +0100

    Fixes

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Contact(models.Model):
     name = models.CharField(_("name"), max_length=250, blank=False)
     email = models.EmailField(_("email"), max_length=250, blank=False)
-    subject = models.CharField(_("message"), max_length=250, blank=False)
+    message = models.CharField(_("message"), max_length=250, blank=False)
 
     def __str__(self):
         return f'{self.name} : {self.subject}'
